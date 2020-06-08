f3c737c95e7af17859e4790ac63fe918efc590f4
Damilare Richard Olagbende <damilareshinaayo@gmail.com>
2020-06-08 22:05:05 +0100

    smooth scroll

---
+++ main.js
@@ -0,0 +1,24 @@
+// jQuery smooth scroll
+
+// $('.navbar a').on('click', function(e){
+
+// 	if(this.hash !== '') {
+// 	e.preventDefault();
+
+// 	const hash = this.hash;
+
+// 	$('html, body').animate({
+// 		scrollTop: $(hash).offset().top
+// 	}, 
+// 	800
+// 	);
+
+// }
+// });
+
+// Option 3- smooth scroll script
+const scroll = new SmoothScroll('.navbar a[href*="#"]',{
+	speed: 300 
+
+});
+
